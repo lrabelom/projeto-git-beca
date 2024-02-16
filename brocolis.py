4c3768c0e73db45cf45a6a5ef90b7bf39d8c817e
laís :) <lrabelom@emeal.nttdata.com>
2024-02-16 11:17:13 -0300

    feat: add brocolis.py file

---
+++ brocolis.py
@@ -0,0 +1,14 @@
+1
+Cozinhe o macarrão normalmente até ficar al dente com um pouco de sal e margarina.
+
+2
+Prepare um molho branco. Leve ao fogo o leite, a margarina e a maizena, mexendo sempre até engrossar (se quiser, após engrossar, acrescente 1 caixinha de creme de leite).
+
+3
+Frite o bacon com o alho e acrescente o brócolis e o cheiro verde.
+
+4
+Despeje o molho branco no brócolis refogado e depois, esta mistura ao macarrão.
+
+5
+Cubra com a mussarela e leve ao forno para derreter.
